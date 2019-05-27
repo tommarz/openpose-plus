a56e9f3b893a1901d069758b3001a4e7fa85c352
Inferics <inferics@DeepLearningBox>
2019-05-27 18:06:05 +0200

    Update train_config.py some test training changes

--- train_config.py
+++ train_config.py
@@ -5,25 +5,30 @@ from easydict import EasyDict as edict
 config = edict()
 
 config.TRAIN = edict()
-config.TRAIN.batch_size = 8
+config.TRAIN.batch_size = 10
 config.TRAIN.save_interval = 2500
-config.TRAIN.log_interval = 1
+config.TRAIN.log_interval = 10
 #config.TRAIN.n_epoch = #10
 config.TRAIN.n_step = 600000  # total number of step
-config.TRAIN.lr_init = 2e-4  #4e-5 # initial learning rate
-config.TRAIN.lr_decay_every_step = 136106  # evey number of step to decay lr
+config.TRAIN.lr_init = 4e-5  #4e-5 # initial learning rate
+config.TRAIN.lr_decay_every_step = 100000  # evey number of step to decay lr
 config.TRAIN.lr_decay_factor = 0.333  # decay lr factor
-config.TRAIN.weight_decay_factor = 5e-4
+config.TRAIN.weight_decay_factor = 5e-4#5e-4
 config.TRAIN.train_mode = 'parallel'  # single, parallel
 
 config.MODEL = edict()
 config.MODEL.model_path = 'models'  # save directory
+config.MODEL.model_file = 'pose.npz'  # save file
 config.MODEL.n_pos = 19  # number of keypoints + 1 for background
 config.MODEL.hin = 368  # input size during training , 240
 config.MODEL.win = 368
 config.MODEL.hout = int(config.MODEL.hin / 8)  # output size during training (default 46)
 config.MODEL.wout = int(config.MODEL.win / 8)
-config.MODEL.name = 'mobilenet'  # vgg, vggtiny, mobilenet
+config.MODEL.name = 'hao28_experimental'  # vgg, vggtiny, mobilenet,hao28_experimental
+
+config.MODEL.initial_weights = False  # True,False
+config.MODEL.initial_weights_file = 'mobilenet.npz'  # save file
+
 
 if (config.MODEL.hin % 16 != 0) or (config.MODEL.win % 16 != 0):
     raise Exception("image size should be divided by 16")
@@ -39,7 +44,7 @@ config.LOG = edict()
 config.LOG.vis_path = 'vis'
 
 config.EVAL = edict()
-config.EVAL.model = 'pose.npz'
+config.EVAL.model = 'pose_best.npz'
 config.EVAL.eval_path = 'eval'
 config.EVAL.data_idx = -1 # data_idx >= 0 to use specified data
 config.EVAL.eval_size = -1 # use first eval_size elements to evaluate, only when data_idx < 0
